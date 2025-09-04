hyperledger-labs/splice

add V043__scan_verdict_store.sql
Signed-off-by: Divam <dfordivam@gmail.com>

apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V043__scan_verdict_store.sql
@@ -1,0 +1,55 @@
+-- Table 1: scan_verdict_store
+create table scan_verdict_store
+(
+    -- Primary key for the table
+    row_id                      bigint generated by default as identity primary key,
+    -- The time when this row was inserted, used for debugging and monitoring
+    ingested_at                 timestamptz not null default now(),
+    -- Migration identifier for domain migrations
+    migration_id                bigint not null,
+    -- Domain identifier
+    domain_id                   text not null,
+    -- When was this verdict sequenced
+    record_time                 bigint not null,
+    -- When was this verdict finalized
+    finalization_time           bigint not null,
+    -- Participant who submitted the verdict
+    submitting_participant_uid  text not null,
+    -- The result of the verdict
+    verdict_result              smallint not null,
+    -- Mediator group identifier
+    mediator_group              integer not null,
+    -- Update identifier for this verdict
+    update_id                   text not null,
+    -- Array of parties that submitted this verdict
+    submitting_parties          text[] not null,
+    -- Array of transaction root view IDs associated with this verdict
+    transaction_root_views      integer[] not null
+);
+
+-- Index for efficient querying by migration, domain, and record time
+create index scan_verdict_mi_di_rt on scan_verdict_store (migration_id, domain_id, record_time);
+create index scan_verdict_di_mi_rt on scan_verdict_store (domain_id, migration_id, record_time);
+
+-- Index for querying by update_id
+create index scan_verdict_update_id on scan_verdict_store (update_id);
+
+-- Table 2: scan_verdict_transaction_view_store
+create table scan_verdict_transaction_view_store
+(
+    -- Foreign key reference to the verdict
+    verdict_row_id              bigint not null references scan_verdict_store (row_id),
+    -- View identifier within the verdict
+    view_id                     integer not null,
+    -- Array of parties that should be informed about this view
+    informees                   text[] not null,
+    -- JSON object containing confirming parties information
+    confirming_parties          jsonb not null,
+    -- Array of sub-view IDs
+    sub_views                   integer[] not null,
+    -- Composite primary key
+    primary key (verdict_row_id, view_id)
+);
+
+-- Index for efficient lookups by verdict_row_id
+create index scan_verdict_tx_view_verdict_id on scan_verdict_transaction_view_store (verdict_row_id);

obsidiansystems/canton-splice

apps/common/src/main/resources/db/migration/canton-network/postgres/stable/V043__scan_verdict_store.sql
@@ -1,0 +1,52 @@
+create table scan_verdict_store
+(
+    -- Primary key for the table
+    row_id                      bigint generated by default as identity primary key,
+    -- The time when this row was inserted, used for debugging and monitoring
+    ingested_at                 timestamptz not null default now(),
+    -- Migration identifier for domain migrations
+    migration_id                bigint not null,
+    -- Domain identifier
+    domain_id                   text not null,
+    -- When was this verdict sequenced
+    record_time                 bigint not null,
+    -- When was this verdict finalized
+    finalization_time           bigint not null,
+    -- Participant who submitted the verdict
+    submitting_participant_uid  text not null,
+    -- The result of the verdict
+    verdict_result              smallint not null,
+    -- Mediator group identifier
+    mediator_group              integer not null,
+    -- Update identifier for this verdict
+    update_id                   text not null,
+    -- Array of parties that submitted this verdict
+    submitting_parties          text[] not null,
+    -- Array of transaction root view IDs associated with this verdict
+    transaction_root_views      integer[] not null
+);
+
+-- Index for efficient querying by migration, and record time
+create index scan_verdict_mi_rt_di on scan_verdict_store (migration_id, record_time, domain_id);
+
+-- Index for querying by update_id
+create index scan_verdict_update_id on scan_verdict_store (update_id);
+
+create table scan_verdict_transaction_view_store
+(
+    -- Foreign key reference to the verdict
+    verdict_row_id              bigint not null references scan_verdict_store (row_id),
+    -- View identifier within the verdict
+    view_id                     integer not null,
+    -- Array of parties that should be informed about this view
+    informees                   text[] not null,
+    -- JSON object containing confirming parties information
+    confirming_parties          jsonb not null,
+    -- Array of sub-view IDs
+    sub_views                   integer[] not null,
+    -- Composite primary key
+    primary key (verdict_row_id, view_id)
+);
+
+-- Index for efficient lookups by verdict_row_id
+create index scan_verdict_tx_view_verdict_id on scan_verdict_transaction_view_store (verdict_row_id);