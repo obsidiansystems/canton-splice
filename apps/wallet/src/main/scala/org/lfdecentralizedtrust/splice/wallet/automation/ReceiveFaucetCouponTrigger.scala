1513514e3da5dec294c9f6330994db9fe7158e2c
Tim Emiola <adetokunbo@users.noreply.github.com>
2025-11-28 19:58:08 +0900

    Make a wallet automation copy of ReceiveFaucetCouponTrigger
    Omitted: behaviour updates make liveness check depend on weight

---
+++ apps/wallet/src/main/scala/org/lfdecentralizedtrust/splice/wallet/automation/ReceiveFaucetCouponTrigger.scala
@@ -0,0 +1,158 @@
+// Copyright (c) 2024 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package org.lfdecentralizedtrust.splice.wallet.automation
+
+import cats.data.OptionT
+import com.digitalasset.canton.logging.pretty.{Pretty, PrettyPrinting}
+import com.digitalasset.canton.time.Clock
+import com.digitalasset.canton.tracing.TraceContext
+import io.opentelemetry.api.trace.Tracer
+import org.apache.pekko.stream.Materializer
+import org.lfdecentralizedtrust.splice.automation.RoundBasedRewardTrigger.RoundBasedTask
+import org.lfdecentralizedtrust.splice.automation.{
+  RoundBasedRewardTrigger,
+  TaskOutcome,
+  TaskSuccess,
+  TriggerContext,
+}
+import org.lfdecentralizedtrust.splice.codegen.java.splice.round.OpenMiningRound
+import org.lfdecentralizedtrust.splice.codegen.java.splice.validatorlicense.ValidatorLicense
+import org.lfdecentralizedtrust.splice.environment.{CommandPriority, SpliceLedgerConnection}
+import org.lfdecentralizedtrust.splice.scan.admin.api.client.BftScanConnection
+import org.lfdecentralizedtrust.splice.util.{AssignedContract, ContractWithState}
+import org.lfdecentralizedtrust.splice.wallet.store.UserWalletStore
+import org.lfdecentralizedtrust.splice.wallet.{UserWalletManager, UserWalletService}
+import org.lfdecentralizedtrust.splice.wallet.util.{TopupUtil, ValidatorTopupConfig}
+import io.grpc.Status
+
+import java.time.temporal.ChronoUnit
+import java.time.Instant
+import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.OptionConverters.*
+import scala.math.Ordering.Implicits.*
+
+class ReceiveFaucetCouponTrigger(
+    override protected val context: TriggerContext,
+    scanConnection: BftScanConnection,
+    store: UserWalletStore,
+    spliceLedgerConnection: SpliceLedgerConnection,
+    validatorTopupConfigO: Option[ValidatorTopupConfig],
+    clock: Clock,
+)(implicit
+    override val ec: ExecutionContext,
+    override val tracer: Tracer,
+    materializer: Materializer,
+) extends RoundBasedRewardTrigger[ReceiveFaucetCouponTrigger.Task] {
+
+  private val endUserParty = store.key.endUserParty
+
+  override protected def retrieveAvailableTasksForRound()(implicit
+      tc: TraceContext
+  ): Future[Seq[ReceiveFaucetCouponTrigger.Task]] =
+    retrieveNextRoundToClaim().value.map(_.toList)
+
+  private def retrieveNextRoundToClaim()(implicit
+      tc: TraceContext
+  ): OptionT[Future, ReceiveFaucetCouponTrigger.Task] = {
+    for {
+      license <- OptionT(
+        store
+          .lookupValidatorLicenseWithOffset()
+          .map(_.value.flatMap(_.toAssignedContract))
+      )
+      (openRounds, _) <- OptionT.liftF(scanConnection.getOpenAndIssuingMiningRounds())
+      firstOpenNotClaimed <- OptionT.fromOption[Future](
+        openRounds
+          .filter(round =>
+            license.payload.faucetState.toScala
+              .forall(
+                _.lastReceivedFor.number < round.payload.round.number
+              ) && round.payload.opensAt <= context.clock.now.toInstant
+          )
+          .minByOption(_.contract.payload.opensAt)
+      )
+    } yield ReceiveFaucetCouponTrigger.Task(license, firstOpenNotClaimed)
+  }
+
+  override protected def completeTask(task: ReceiveFaucetCouponTrigger.Task)(implicit
+      tc: TraceContext
+  ): Future[TaskOutcome] = {
+    val ReceiveFaucetCouponTrigger.Task(license, unclaimedRound) = task
+    license.payload.faucetState.toScala
+      .map(_.lastReceivedFor.number.longValue()) match {
+      case None =>
+        logger.info(
+          s"Validator never received faucet coupons, it will start now at round ${unclaimedRound.payload.round.number}."
+        )
+      case Some(lastReceivedFor) =>
+        val skippedCount = unclaimedRound.payload.round.number - lastReceivedFor - 1
+        if (skippedCount > 0)
+          logger.warn(
+            s"Skipped $skippedCount faucet coupons from last claimed round $lastReceivedFor to current round ${unclaimedRound.payload.round.number}. " +
+              s"This is expected in case of validator inactivity."
+          )
+    }
+    for {
+      commandPriority <- validatorTopupConfigO match {
+        case None =>
+          Future.successful(CommandPriority.Low) // not the wallet of the validator operator
+        case Some(validatorTopupConfig) =>
+          TopupUtil
+            .hasSufficientFundsForTopup(scanConnection, store, validatorTopupConfig, clock)
+            .map(if (_) CommandPriority.Low else CommandPriority.High): Future[CommandPriority]
+      }
+      outcome <- spliceLedgerConnection
+        .submit(
+          actAs = Seq(endUserParty),
+          readAs = Seq(endUserParty),
+          license.exercise(
+            _.exerciseValidatorLicense_RecordValidatorLivenessActivity(
+              unclaimedRound.contractId
+            )
+          ),
+          priority = commandPriority,
+        )
+        .noDedup
+        .withDisclosedContracts(spliceLedgerConnection.disclosedContracts(unclaimedRound))
+        .yieldUnit()
+        .map(_ =>
+          TaskSuccess(s"Received faucet coupon for Round ${unclaimedRound.payload.round.number}")
+        )
+    } yield outcome
+  }
+
+  override protected def isStaleTask(task: ReceiveFaucetCouponTrigger.Task)(implicit
+      tc: TraceContext
+  ): Future[Boolean] = {
+    val nextRound = retrieveNextRoundToClaim()
+    nextRound.forall(_ != task)
+  }
+
+}
+
+object ReceiveFaucetCouponTrigger {
+
+  case class Task(
+      license: AssignedContract[ValidatorLicense.ContractId, ValidatorLicense],
+      round: ContractWithState[OpenMiningRound.ContractId, OpenMiningRound],
+  ) extends PrettyPrinting
+      with RoundBasedTask {
+    import org.lfdecentralizedtrust.splice.util.PrettyInstances.*
+    override def pretty: Pretty[this.type] =
+      prettyOfClass(
+        param("license", _.license),
+        param("round", _.round),
+      )
+
+    override def scheduleAtMaxTargetTime: Instant =
+      opensAt.plus(round.payload.tickDuration.microseconds, ChronoUnit.MICROS)
+
+    def closesAt: Instant = round.payload.targetClosesAt
+
+    def roundNumber: Long = Long.unbox(round.payload.round.number)
+
+    def opensAt: Instant = round.payload.opensAt
+  }
+
+}
