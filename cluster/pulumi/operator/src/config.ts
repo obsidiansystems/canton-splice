f03536b00b7b9a2198a7f383b318a8c13c80ad5b
Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>
2026-07-10 13:40:24 +0200

    make SLACK_ALERT_NOTIFICATION_CHANNEL_FULL_NAME loading lazy to fix operator on non-prod-like clusters (#6381)
    [static]
    
    Signed-off-by: Mateusz Błażejewski <mateusz.blazejewski@digitalasset.com>

--- cluster/pulumi/operator/src/config.ts
+++ cluster/pulumi/operator/src/config.ts
@@ -8,10 +8,16 @@ export const OperatorDeploymentConfigSchema = z.object({
   reference: GitReferenceSchema,
   flux: z
     .object({
-      alertSlackChannel: z
-        .string()
-        .default(() => config.requireEnv('SLACK_ALERT_NOTIFICATION_CHANNEL_FULL_NAME')),
+      alertSlackChannel: z.string().optional(),
     })
+    .transform(flux => ({
+      ...flux,
+      get alertSlackChannel(): string {
+        return (
+          flux.alertSlackChannel ?? config.requireEnv('SLACK_ALERT_NOTIFICATION_CHANNEL_FULL_NAME')
+        );
+      },
+    }))
     .prefault({}),
 });
 
